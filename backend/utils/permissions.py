6fce86554cd477b5a79bbde2399bc0eefe42f23d
chenkun1998 <chenkun1998@163.com>
2018-09-06 01:10:54 +0800

    防止越级权限

--- backend/utils/permissions.py
+++ backend/utils/permissions.py
@@ -36,40 +36,36 @@ class AuthOrReadOnly(AppellationMixins, permissions.BasePermission):
     def has_permission(self, request, view):
         uri_list = request.META['PATH_INFO'].split('/')
         uri = uri_list[-2]
-        if uri not in handle_perms + approve_perms:  # 非动作的操作（查询类的）
+        if uri not in handle_perms + approve_perms: 
             return True
         pk = uri_list[-3]
         sqlobj = Inceptsql.objects.get(pk = pk)
-        if sqlobj.env == self.env_test:  # 测试环境
+        if sqlobj.env == self.env_test: 
             return True
-        return uri in self.get_permission[request.user.role] or request.user.is_superuser  # 用户的身份有操作权限 or 是超级用户
-        # return sqlobj.treater == request.user.username or request.user.is_superuser  # SQL的执行人是用户 or 是超级用户
+        return uri in self.get_permission[request.user.role] or request.user.is_superuser 
 
 class IsHandleAble(AppellationMixins, permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         """
             在调get_object()时检查该权限（get_object -- check_object_permissions -- has_object_permission）
         """
-        print(obj)
-        #if obj.env == self.env_test or obj.is_manual_review == False:
-         #   return True
-        user_id = request.user.id  # 当前访问者
+        user_id = request.user.id 
         if obj.is_manual_review == True:
             approve_step_instance = obj.step_set.all()[1]
-            approve_user_id = approve_step_instance.user.id  # 工单审批人
+            approve_user_id = approve_step_instance.user.id 
         uri_list = request.META['PATH_INFO'].split('/')
         uri = uri_list[-2]
         if uri in handle_perms:  # 执行/回滚的检查项
             if obj.is_manual_review == False:
                 return True
             else:
-                if user_id == approve_user_id:  # 最终执行人与审批人，不能是同一人
+                if user_id == approve_user_id: 
                     return False
-                return True if obj.handleable else False # 检查工单是否审批通过
+                return True if obj.handleable else False 
         if uri in approve_perms:  # 审批的检查项
-            if obj.env == self.env_test or obj.is_manual_review == False:  # 测试环境或没有审批流的生产环境，无审批权限
+            if obj.env == self.env_test or obj.is_manual_review == False: 
                 return False
-            if user_id != approve_user_id:  # 审批人和当前操作人，必须是同一人
+            if user_id != approve_user_id: 
                 return False
         return True
 
