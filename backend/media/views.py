a002d155f96471a7ed096641b300d6cf3e58845b
chen kun <chenkun1998@163.com>
2018-10-29 11:50:02 +0800

    Update views.py

--- backend/media/views.py
+++ backend/media/views.py
@@ -3,13 +3,12 @@ from .mixins import DownloadBaseView
 
 # Create your views here.
 class SqlFileView(DownloadBaseView):
-    
+    '''
+        文件下载
+    '''
     model = Inceptsql
     
     def get_content(self):
-        '''
-            文件下载
-        '''
         pk = self.kwargs.get('pk')
         content = self.model.objects.get(pk=pk).handle_result
         return content
