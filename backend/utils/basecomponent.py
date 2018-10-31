d0dddd5bfcdeb8f0cf32225aeff03691869ce5c1
chen kun <chenkun1998@163.com>
2018-10-31 17:59:30 +0800

    Update basecomponent.py

--- backend/utils/basecomponent.py
+++ backend/utils/basecomponent.py
@@ -7,7 +7,7 @@ class DateEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, datetime.datetime):
             return  obj.strftime('%Y-%m-%d %H:%M:%S')
-        elif isinstance(obj, date):
+        elif isinstance(obj, datetime.date):
             return obj.strftime("%Y-%m-%d")
         else:
             return json.JSONEncoder.default(self, obj)
