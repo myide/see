c690db9acf003f54b8be38a495414043e6b0cccc
chenkun1998@163.com <chenkun1998@163.com>
2019-01-10 23:49:00 -0500

    更新测试脚本

--- backend/utils/inception_test.py
+++ backend/utils/inception_test.py
@@ -4,33 +4,30 @@
 ''' 建表语句
 CREATE DATABASE pro1;
 CREATE TABLE IF NOT EXISTS pro1.mytable1 (
-   `id` INT UNSIGNED AUTO_INCREMENT,
-   `myname` VARCHAR(10) NOT NULL,
+   `id` INT UNSIGNED AUTO_INCREMENT comment "用户id",
+   `myname` VARCHAR(10) DEFAULT "" NOT NULL comment "用户name",
    PRIMARY KEY ( `id` )
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 comment="表的注释";
 
 '''
 
 import pymysql
 
 # 待审核/执行的sql语句（需包含目标数据库的地址、端口 等参数）
-sql='/* --user=root;--password=123456;--host=127.0.0.1;--port=3306;--enable-execute; */\
+sql = '/* --user=root;--password=123456;--host=127.0.0.1;--port=3306;--enable-execute; */\
 inception_magic_start;\
 use pro1;\
 insert into mytable1 (myname) values ("xianyu1"),("xianyu2");\
 insert into mytable1 (myname) values ("xianyu1"),("xianyu2");\
 inception_magic_commit;'
 try:
-    conn=pymysql.connect(host='127.0.0.1',user='',passwd='',db='',port=6669,use_unicode=True,charset="utf8")  # inception的地址、端口等
-    cur=conn.cursor()
-    ret=cur.execute(sql)
-    result=cur.fetchall()
-    num_fields = len(cur.description) 
-    field_names = [i[0] for i in cur.description]
-    print(result)
+    conn = pymysql.connect(host='127.0.0.1',user='',passwd='',db='',port=6669,use_unicode=True,charset="utf8")  # inception的地址、端口等
+    cur = conn.cursor()
+    ret = cur.execute(sql)
+    result = cur.fetchall()
     cur.close()
     conn.close()
+    for res in result:
+        print(res)
 except pymysql.Error as e:
-     print("Mysql Error %d: %s" % (e.args[0], e.args[1]) )
-
-
+    print("Mysql Error %d: %s" % (e.args[0], e.args[1]) )
