a2677b8f039e32d3a9441e79d841490113efac0e
chenkun1998@163.com <chenkun1998@163.com>
2019-04-19 13:06:06 +0800

    订阅只显示有权限的数据库

--- backend/sqlmng/views/select_data.py
+++ backend/sqlmng/views/select_data.py
@@ -15,11 +15,22 @@ class SelectDataView(AppellationMixin, BaseView):
     serializer_class = DbSerializer
     serializer_user = UserSerializer
 
+    def get_permission_databases(self):
+        user = self.request.user
+        group = user.groups.first()
+        user_perms = user.userobjectpermission_set.all()
+        group_perms = group.groupobjectpermission_set.all() if group else []
+        user_perms_set = set([perm.object_pk for perm in user_perms])
+        group_perms_set = set([perm.object_pk for perm in group_perms])
+        return user_perms_set | group_perms_set
+
     def create(self, request, *args, **kwargs):
         ret = res.get_ret()
         env = request.data.get('env')
         cluster = request.data.get('cluster') or None
         qs_db = self.queryset.filter(env=env, cluster_id=cluster)
+        perm_dbs = self.get_permission_databases()
+        qs_db = [db for db in qs_db if str(db.id) in perm_dbs]
         qs_admin = self.serializer_user.Meta.model.objects.filter(is_superuser=True)
         ret['data']['dbs'] = self.serializer_class(qs_db, many=True).data
         ret['data']['admins'] = self.serializer_user(qs_admin, many=True).data
