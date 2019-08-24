791893f3c00f5a7ffb2f37d18c9f792633cfe95e
chen kun <chenkun1998@163.com>
2019-08-24 11:53:20 +0800

    Update models.py
    去掉级联删除

--- backend/account/models.py
+++ backend/account/models.py
@@ -8,8 +8,8 @@ class User(AbstractUser):
         ('developer_manager', u'经理'),
         ('developer', u'研发'),
     )
-    leader = models.ForeignKey('User', null=True, blank=True, on_delete=models.CASCADE, related_name='leader_devs')
-    admin_mail = models.ForeignKey('User', null=True, blank=True, on_delete=models.CASCADE, related_name='admin_devs')
+    leader = models.ForeignKey('User', null=True, blank=True, on_delete=models.SET_NULL, related_name='leader_devs')
+    admin_mail = models.ForeignKey('User', null=True, blank=True, on_delete=models.SET_NULL, related_name='admin_devs')
     role = models.CharField(max_length=32, default='developer', choices=ROLES)
     mail_list_extend = models.TextField(null=True, blank=True)
     remark = models.CharField(max_length=128, default='', blank=True)
